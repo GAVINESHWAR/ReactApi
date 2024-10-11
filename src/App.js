f8f2eeb6d550aa00b075312f89b433873bc6b7c8
avi <avineshwarthegreat@gmail.com>
2024-10-11 21:39:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Fetching Pokemon Data from API
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
+        const promises = response.data.results.map(async (pokemon) => {
+          const pokemonDetails = await axios.get(pokemon.url);
+          return pokemonDetails.data;
+        });
+        const data = await Promise.all(promises);
+        setPokemonData(data);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
+
+    fetchPokemonData();
+  }, []);
+
+  // Handle search functionality
+  const filteredPokemon = pokemonData.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Pokémon</h1>
+      <input
+        type="text"
+        placeholder="Search Pokémon"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+      />
+      <div className="card-container">
+        {filteredPokemon.map((pokemon) => (
+          <div key={pokemon.id} className="pokemon-card">
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <h2>{pokemon.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
